b8e210fbedbd657bc5cedc40202aedd234460df9
Andreyinthesky <and19980316@gmail.com>
2020-02-26 20:11:30 +0500

    implement ranked algo

--- src/testScheme.js
+++ src/testScheme.js
@@ -49,22 +49,62 @@ const not = new Not([false], [{ inputIndex: 1, el: or }, { inputIndex: 0, el: ou
 const in1 = new Input([true], [{ inputIndex: 0, el: or }]);
 const in2 = new Input([true], [{ inputIndex: 0, el: not }]);
 
-console.log("Success");
+const elements = [out, out2, or, not, in1, in2];
 
-console.log("start eval");
+function evalScheme(rankedElements) {
+  console.log("start eval");
+  const maxRank = rankedElements.length - 1;
 
-const queue = [in1, in2];
+  for (let rank = 0; rank <= maxRank; rank++) {
+    rankedElements[rank].forEach(element => {
+      const outputs = element.output;
 
-while (queue.length > 0) {
-  const current = queue.shift();
-  const output = current.output;
-  output && output.forEach(outputObj => {
-    const output = outputObj.el;
-    output.input[outputObj.inputIndex] = current.eval();
-    queue.push(output);
-  });
+      outputs && outputs.forEach(outputObj => {
+        const output = outputObj.el;
+        output.input[outputObj.inputIndex] = element.eval();
+      });
+    });
+  }
+
+  console.log("end eval");
 }
 
-console.log("end eval");
+function rankElements() {
+  const queue = [in1, in2];
 
-console.log(out.input, out2.input);
+  elements.forEach(element => element.rank = null);
+  queue.forEach(element => element.rank = 0);
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+    const output = current.output;
+
+    output && output.forEach(outputObj => {
+      const output = outputObj.el;
+
+      if (output.rank < current.rank + 1) {
+        output.rank = current.rank + 1;
+      }
+
+      queue.push(output);
+    });
+  }
+
+  const rankedElements = [];
+  elements.forEach(element => {
+    const rank = element.rank;
+
+    if (!rankedElements[rank])
+      rankedElements[rank] = [];
+
+    rankedElements[rank].push(element);
+  });
+
+  return rankedElements;
+}
+
+const rankedElements = rankElements();
+
+evalScheme(rankedElements);
+
+console.log(out, out2);
