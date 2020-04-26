1d09974ee6fdb7ebef7b5396c29433e061816003
Andreyinthesky <and19980316@gmail.com>
2020-04-26 12:56:45 +0500

    Header.jsx fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import LoadSchemeForm from "./LoadSchemeForm";
 
-export default class App extends Component {
+export default class Header extends Component {
     constructor() {
         super();
     }
