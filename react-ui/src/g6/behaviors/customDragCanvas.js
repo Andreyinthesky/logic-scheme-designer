3eed53fe8a366519a83992843421d1927f1241d3
Andreyinthesky <and19980316@gmail.com>
2020-08-06 00:18:45 +0500

    make sync grid position as graph patch

--- react-ui/src/g6/behaviors/customDragCanvas.js
+++ react-ui/src/g6/behaviors/customDragCanvas.js
@@ -31,7 +31,7 @@ const customDragCanvasBehavior = {
     }
 
     this.translateCanvasByDrag({ x: evt.clientX, y: evt.clientY });
-    this.syncGridPositionByCanvas();
+    // this.syncGridPositionByCanvas();
   },
   startDrag(startMouseCoord) {
     this.startMouseCoord = startMouseCoord;
@@ -53,13 +53,13 @@ const customDragCanvasBehavior = {
     graph.translate(xOffset, yOffset);
     this.startMouseCoord = currentMouseCoords;
   },
-  syncGridPositionByCanvas() {
-    const { graph } = this;
-    const { x: xOrigin, y: yOrigin } = graph.getCanvasByPoint(0, 0);
-    const scale = graph.getZoom();
-    const gridPosition = { x: xOrigin / scale, y: yOrigin / scale };
-    document.querySelector(".g6-grid").style.backgroundPosition = `${gridPosition.x}px ${gridPosition.y}px`;
-  }
+  // syncGridPositionByCanvas() {
+  //   const { graph } = this;
+  //   const { x: xOrigin, y: yOrigin } = graph.getCanvasByPoint(0, 0);
+  //   const scale = graph.getZoom();
+  //   const gridPosition = { x: xOrigin / scale, y: yOrigin / scale };
+  //   document.querySelector(".g6-grid").style.backgroundPosition = `${gridPosition.x}px ${gridPosition.y}px`;
+  // }
 };
 
 export default customDragCanvasBehavior;
--- react-ui/src/model/g6Init.js
+++ react-ui/src/model/g6Init.js
@@ -1,8 +1,25 @@
 import G6 from "../g6";
 import Grid from "../../lib/g6/grid";
 
-import EditorObjIndexer from "./schemeEditor/indexer";
+import EditorObjIndexer from "./schemeEditor/ItemIndexer";
 import { EDITOR_EDITING_MODE, EDITOR_SIMULATION_MODE } from "./constants";
+
+
+function syncGridPositionByGraphTranlatePatch(graph) {
+  const oldTranslateFn = graph.translate;
+
+  function syncGridPositionByCanvas(graph) {
+    const { x: xOrigin, y: yOrigin } = graph.getCanvasByPoint(0, 0);
+    const scale = graph.getZoom();
+    const gridPosition = { x: xOrigin / scale, y: yOrigin / scale };
+    document.querySelector(".g6-grid").style.backgroundPosition = `${gridPosition.x}px ${gridPosition.y}px`;
+  }
+
+  graph.translate = function (x, y) {
+    oldTranslateFn.call(graph, x, y);
+    syncGridPositionByCanvas(graph);
+  }
+}
 
 
 export default function init(mountNode) {
@@ -17,13 +34,13 @@ export default function init(mountNode) {
       type: "schemeLayout"
     },
     modes: {
-      // [EDITOR_EDITING_MODE]: ["drag-node", "items-control", "custom-drag-canvas"],
-      // [EDITOR_SIMULATION_MODE]: ["change-input-state", "custom-drag-canvas", "drag-node"],
       [EDITOR_EDITING_MODE]: ["custom-drag-node", "items-control", "custom-drag-canvas"],
       [EDITOR_SIMULATION_MODE]: ["change-input-state", "custom-drag-canvas", "custom-drag-node"],
     },
     plugins: [new Grid()]
   });
+
+  syncGridPositionByGraphTranlatePatch(graph);
 
   graph.indexer = new EditorObjIndexer();
   graph.setMode(EDITOR_EDITING_MODE);
